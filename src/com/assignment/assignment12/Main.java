b744a2077c20ebff2ccc5dace67d80dff645421c
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2021-11-24 12:16:19 +0530

    updated assignment

--- src/com/assignment/assignment12/Main.java
+++ src/com/assignment/assignment12/Main.java
@@ -4,18 +4,37 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args){
-        Stream<Student> students = StudentDetails.getStudentsList();
-        StreamCall streamCall = new StreamCall(students);
-       // streamCall.printDepartment();
-       // streamCall.printEnrollAfter(2018);
-       // streamCall.printAllMalesInCS();
-        //streamCall.printTotalNumberOfMaleAndFemale();
-        streamCall.printAverageAge("Male");
-        streamCall.printAverageAge("Female");
+
+        StreamCall streamCall = new StreamCall();
+        System.out.println(" Print the name of all departments in the college");
+        streamCall.printDepartment();
+        System.out.println();
+        System.out.println("the names of all students who have enrolled after 2018");
+        streamCall.printEnrollAfter(2018);
+        System.out.println();
+        System.out.println("details of all male student in the computer sci department");
+        streamCall.printAllMalesInCS();
+        System.out.println();
+        System.out.println("Total male and female student ");
+        streamCall.printTotalNumberOfMaleAndFemale();
+        System.out.println();
+        System.out.println(" the average age of male and female students");
+        streamCall.printAverageAge();
+        System.out.println();
+        System.out.println("the details of highest student having highest percentage ");
         streamCall.printHighestPercentage();
+        System.out.println();
+        System.out.println("Count the number of students in each department");
         streamCall.printNumberOfStudentsInEachDepartment();
+        System.out.println();
+        System.out.println("the average percentage achieved in each department");
         streamCall.printAveragePercentageInDepartments();
+        System.out.println();
+        System.out.println("the details of youngest male student in the Electronic department");
         streamCall.printYoungestMaleInElectronicDepartment();
+        System.out.println();
+        System.out.println("Total  male and female students are there in the computer science department");
+        streamCall.printCountOfMaleAndFemaleInCS();
 
     }
 }
--- src/com/assignment/assignment12/StreamCall.java
+++ src/com/assignment/assignment12/StreamCall.java
@@ -11,37 +11,34 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamCall {
-    private final Stream<Student> studentStream;
-    StreamCall(Stream<Student> studentStream){
-        this.studentStream = studentStream;
+    private  Supplier<Stream<Student>> supplierStreamStudent ;
+    StreamCall(){
+        this.supplierStreamStudent=()->StudentDetails.getStudentsList();
     }
     public void printDepartment(){
-        Stream<String> departments = studentStream.map(Student::getEngDepartment).distinct();
+        Stream<String> departments = supplierStreamStudent.get().map(Student::getEngDepartment).distinct();
         departments.forEach(System.out::println);
     }
     public void printEnrollAfter(int year){
-        Stream<String> studentName = studentStream.filter(student ->
+        Stream<String> studentName = supplierStreamStudent.get().filter(student ->
              student.getYearOfEnrollment()>year
         ).map(Student::getName);
         studentName.forEach(System.out::println);
     }
     public void printAllMalesInCS(){
-        Stream<String> maleStudentInCS = studentStream.filter(student ->
+        Stream<String> maleStudentInCS = supplierStreamStudent.get().filter(student ->
             student.getGender().equals("Male") && student.getEngDepartment().equals("Computer Science")
         ).map(Student::getName);
         maleStudentInCS.forEach(System.out::println);
     }
     public void printTotalNumberOfMaleAndFemale(){
-        Map<String,Long> maleAndFemaleCount = studentStream.collect(Collectors.groupingBy(Student::getGender,Collectors.counting()));
+        Map<String,Long> maleAndFemaleCount = supplierStreamStudent.get().collect(Collectors.groupingBy(Student::getGender,Collectors.counting()));
         System.out.println(maleAndFemaleCount);
     }
 
-    public void printAverageAge(String gender){
-        OptionalDouble average = studentStream.filter(student -> student.getGender().equals(gender))
-                .map(Student::getAge).mapToInt(age->age).average();
-        if(average.isPresent())
-            System.out.println(average.getAsDouble());
-        /*Supplier<Stream<Student>> supplierStreamStudent = ()->StudentDetails.getStudentsList();
+    public void printAverageAge(){
+
+
         OptionalDouble averageAgeOfMale = supplierStreamStudent.get().filter(student ->
              student.getGender().equals("Male")
         ).map(Student::getAge).mapToInt(age->age).average();
@@ -56,24 +53,29 @@ public class StreamCall {
        if(averageAgeOfFemale.isPresent())
            System.out.println(averageAgeOfFemale.getAsDouble());
 
-         */
+
     }
     public void printHighestPercentage(){
-       double highestPercentage = studentStream.map(Student::getPerTillDate).max(Double::compare).get();
+       double highestPercentage = supplierStreamStudent.get().map(Student::getPerTillDate).max(Double::compare).get();
         System.out.println(highestPercentage);
     }
     public void printNumberOfStudentsInEachDepartment(){
-        Map<String,Long> groupByDepartment = studentStream.collect(Collectors.groupingBy(Student::getEngDepartment,Collectors.counting()));
+        Map<String,Long> groupByDepartment = supplierStreamStudent.get().collect(Collectors.groupingBy(Student::getEngDepartment,Collectors.counting()));
         System.out.println(groupByDepartment);
     }
     public void printAveragePercentageInDepartments(){
-        Map<String,Double> averagePercentageInDepartments = studentStream.collect(Collectors.groupingBy(Student::getEngDepartment,Collectors.averagingDouble(Student::getPerTillDate)));
+        Map<String,Double> averagePercentageInDepartments = supplierStreamStudent.get().collect(Collectors.groupingBy(Student::getEngDepartment,Collectors.averagingDouble(Student::getPerTillDate)));
         System.out.println(averagePercentageInDepartments);
     }
     public void printYoungestMaleInElectronicDepartment(){
-        Student youngest = studentStream
+        Student youngest =supplierStreamStudent.get()
                 .filter(student -> student.getEngDepartment().equals("Electronic") && student.getGender().equals("Male"))
                 .min(Comparator.comparing(Student::getAge)).get();
         System.out.println(youngest.getName());
+    }
+    public void printCountOfMaleAndFemaleInCS(){
+        Map<String,Long> totalMaleAndFemaleInCS  =supplierStreamStudent.get().filter(student -> student.getEngDepartment().equals("Computer Science"))
+                .collect(Collectors.groupingBy(Student::getGender,Collectors.counting()));
+        System.out.println(totalMaleAndFemaleInCS);
     }
 }
